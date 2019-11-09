3857cf35ca4f0f3cf0e4a573d5c5e8a629033220
Caleb Maftei <calebmaftei15@gmail.com>
2019-11-08 23:11:31 -0700

    Added shells of all necessary classes

--- cmaftei_Corsi_Span/cmaftei_Corsi_Span/Corsi_Span.Designer.cs
+++ cmaftei_Corsi_Span/cmaftei_Corsi_Span/Corsi_Span.Designer.cs
@@ -30,12 +30,12 @@
         {
             this.SuspendLayout();
             // 
-            // Form1
+            // Corsi_Span
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(1238, 691);
+            this.Name = "Corsi_Span";
             this.Text = "Corsi Span";
             this.ResumeLayout(false);
 
---
+++ cmaftei_Corsi_Span/cmaftei_Corsi_Span/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cmaftei_Corsi_Span
+{
+    class User
+    {
+    }
+}
